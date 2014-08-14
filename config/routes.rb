a6314605c94c9898358a4cce292ceb3884f47793
golpher <gareth.orley@gmail.com>
2014-08-14 11:22:36 +1000

    create sample, remote demoA

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :demo_as
+  resources :samples
 
   resources :events
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814011828) do
+ActiveRecord::Schema.define(version: 20140814012146) do
 
   create_table "boat_types", force: true do |t|
     t.datetime "created_at"
@@ -64,6 +64,14 @@ ActiveRecord::Schema.define(version: 20140814011828) do
   end
 
   create_table "sailors", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "samples", force: true do |t|
+    t.string   "name"
+    t.string   "type"
+    t.integer  "demo_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
